531c72f37e3d74d138d33cbd57d2237c440eff13
ramashish1 <78415382+ramashish1@users.noreply.github.com>
2024-05-02 10:35:09 +0000

    datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144234
+let accountEmail="ashish123@gmail.com"
+var accountPassword ="12345"
+ accountCity="bihar"
+
+
+accountEmail="ashish125@gmail.com"
+accountPassword="21135434"
+accountCity="delhi"
+
+console.log(accountId);
+console.table ([accountEmail,accountPassword,accountCity])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict "; //treat all js code as newer version or newer js
+ 
+// alert("") // we are using node not browser
+
+//string=>""
+//boolean=>true/false
+//null=> stand alone value
+//undefined=>
+//symbol => unique
+//object 
+
+//console.log(typeof"ashish");
+
+//console.log(typeof age);
+
+console.log(typeof null);
