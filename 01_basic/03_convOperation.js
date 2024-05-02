b826219c8d7bf23ca0e269a0dbfed3470f827788
ramashish1 <78415382+ramashish1@users.noreply.github.com>
2024-05-02 11:21:09 +0000

    conversion

---
+++ 01_basic/03_convOperation.js
@@ -0,0 +1,29 @@
+let score = null
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN 
+//true=>1 : false=>0 
+
+// let isLoggedIn= true 
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false=> false 
+//""=>false
+//"ashish"=>true
+
+let SomeNumber=33 
+
+let StringNumber=String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
